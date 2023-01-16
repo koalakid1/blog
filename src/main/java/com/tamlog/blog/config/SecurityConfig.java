73c03e2a91899488bef72661114e897a313f0e37
koalakid1 <koalakid154@gmail.com>
2023-01-17 08:59:18 +0900

    security OAuth2 적용

---
+++ src/main/java/com/tamlog/blog/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.tamlog.blog.config;
+
+import com.tamlog.blog.config.auth.CustomOauth2UserService;
+import com.tamlog.blog.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOauth2UserService customOauth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/api/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                    .userInfoEndpoint()
+                    .userService(customOauth2UserService);
+    }
+}
---
+++ src/main/java/com/tamlog/blog/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.tamlog.blog.config.auth.dto;
+
+import com.tamlog.blog.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String path;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.path = user.getPath();
+    }
+}
