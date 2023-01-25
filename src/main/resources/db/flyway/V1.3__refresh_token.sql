391f8cb78b508155bd282325ea343e44e233a14e
koalakid1 <koalakid154@gmail.com>
2023-01-25 19:12:28 +0900

    refresh token 테이블 추가

--- src/main/resources/db/flyway/V1.3__refresh_token.sql
+++ src/main/resources/db/flyway/V1.3__refresh_token.sql
@@ -1,5 +1,5 @@
 CREATE TABLE refresh_token (
-       rt_id SERIAL PRIMARY KEY,
+       refresh_token_id SERIAL PRIMARY KEY,
        refresh_token VARCHAR(255) NOT NULL,
        account_id INT NOT NULL,
        expires_at TIMESTAMP NOT NULL,
